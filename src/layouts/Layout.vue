fd10962c426a8de23b4d992791b422055e232ab4
max <max.09@outlook.com>
2022-09-28 01:09:12 +0200

    trying to hide new update button...

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -16,9 +16,9 @@
                 <span class="fs-4 title">{{ $t("Diamond Monitoring") }}</span>
             </router-link>
 
-            <!-- <a v-if="hasNewVersion" target="_blank" href="https://github.com/louislam/uptime-kuma/releases" class="btn btn-info me-3">
+            <a v-if="false" target="_blank" href="https://github.com/louislam/uptime-kuma/releases" class="btn btn-info me-3">
                 <font-awesome-icon icon="arrow-alt-circle-up" /> {{ $t("New Update") }}
-            </a> -->
+            </a>
 
             <ul class="nav nav-pills">
                 <li v-if="$root.loggedIn" class="nav-item me-2">
